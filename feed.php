0c9a18aaebdbd6a665d567c44e2969aef539385d
Roumen Damianoff <roumen@dawebs.com>
2012-09-16 20:40:37 +0300

    phpdoc updates

--- feed.php
+++ feed.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Feed generator class for laravel-feed bundle.
- * 
+ *
  * @author Roumen Damianoff <roumen@dawebs.com>
- * @version 1.2
- * @link https://github.com/RoumenMe/laravel-feed GitHub
+ * @version 1.2.1
+ * @link http://roumen.me/projects/laravel-feed
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
 
@@ -18,9 +18,17 @@ class Feed
     public $pubdate;
     public $lang;
 
-    
+
     /**
      * Add new item to $items array
+     *
+     * @param string $title
+     * @param string $author
+     * @param string $link
+     * @param string $pubdate
+     * @param string $description
+     *
+     * @return void
      */
     public function add($title, $author, $link, $pubdate, $description)
     {
@@ -36,13 +44,17 @@ class Feed
 
     /**
      * Returns aggregated feed with all items from $items array
+     *
+     * @param string $format (options: 'atom', 'rss')
+     *
+     * @return view
      */
     public function render($format = 'atom')
     {
         if (empty($this->lang)) $this->lang = Config::get('application.language');
         if (empty($this->link)) $this->link = Config::get('application.url');
         if (empty($this->pubdate)) $this->pubdate = date('D, d M Y H:i:s O');
-        
+
         $channel = array(
             'title'=>$this->title,
             'description'=>$this->description,
@@ -50,7 +62,7 @@ class Feed
             'pubdate'=>$this->pubdate,
             'lang'=>$this->lang
         );
-        
+
         return Response::make(Response::view('feed::'.$format, array('items' => $this->items, 'channel' => $channel) ), 200, array('Content-type' => 'text/xml; charset=utf-8'));
     }
 
